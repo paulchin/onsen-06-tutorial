1ddc91539213d6d22c05a08396271fff347c7f92
paulchin <paulchin20@yahoo.com>
2019-04-17 00:49:35 +0800

    'u'

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -1,10 +1,23 @@
+//--- called from login.html ---
 function login() {
   var username = $('#username').val();
   var password = $('#password').val();
 
-  if (username === 'user' && password === 'pass') {
-    ons.notification.alert('Correct!');
+  if (username === '' && password === '') {
+    var navigator = $('#navigator')[0];
+    navigator.resetToPage('home.html');
   } else {
-    ons.notification.alert('Wrong');
+    ons.notification.toast('Wrong', { timeout: 1000 });
   }
+}
+
+//--- called from home.html ---
+function openMenu() {
+  $('#menu')[0].open();
+}
+
+//--- called from index.html spliter ---
+function loadPage(page) {
+  $('#menu')[0].close();
+  $('#navigator')[0].bringPageTop(page, { animation: 'fade' });
 }
