91143fa7005a43392b13615be1d71bddf4f43940
paulchin <paulchin20@yahoo.com>
2019-04-17 12:11:39 +0800

    'u'

--- www/app.js
+++ www/app.js
@@ -21,3 +21,23 @@ function loadPage(page) {
   $('#menu')[0].close();
   $('#navigator')[0].bringPageTop(page, { animation: 'fade' });
 }
+
+//--- called from home.html to change toolbar title of save.html ---
+document.addEventListener('prechange', function({ target, tabItem }) {
+  if (target.matches('#tabbar')) {
+    $('#home-toolbar .center').html(tabItem.getAttribute('label'));
+  }
+});
+
+//--- called from pokemon.html ---
+let savedPokemon = [];
+
+const addPokemonToGrid = pokenumber => {
+  // we save a list so we can pass it to the gallery
+  savedPokemon.push(pokenumber);
+};
+
+const savePokemon = (pokenumber, button) => {
+  addPokemonToGrid(pokenumber);
+  button.parentNode.parentNode.hideExpansion();
+};
